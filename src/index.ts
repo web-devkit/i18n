fa6cbbc0f6f2387bb92eb4fe0d5a890baa57773a
Marc Binz <code@marcbinz.me>
2026-04-08 22:52:05 +0200

    feat: add display component (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Currency } from "./currency.js";
 export { Datetime } from "./datetime.js";
+export { Display } from "./display-name.js";
 export { Duration } from "./duration.js";
 export { List } from "./list.js";
 export { NumberFormat } from "./number.js";
