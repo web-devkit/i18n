7dc14165c26b8cbd43183ca0aee277a9b5cb4350
Marc Binz <code@marcbinz.me>
2026-04-08 20:17:19 +0200

    feat: add display component

---
+++ src/display-name.ts
@@ -0,0 +1,48 @@
+import { nothing, PropertyValues } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { I18nBase } from "./base.js";
+
+type DisplayNameType = "language" | "region" | "script" | "currency" | "calendar" | "dateTimeField";
+
+@customElement("i18n-display")
+export class Display extends I18nBase {
+    @property() type: DisplayNameType = "language";
+
+    @property() display?: "long" | "short" | "narrow";
+
+    @state() private _formatter?: Intl.DisplayNames;
+
+    override render() {
+        if (!this.value || !this._formatter) return nothing;
+        try {
+            return this._formatter.of(this.value) ?? nothing;
+        } catch {
+            return nothing;
+        }
+    }
+
+    override updated(_changedProperties: PropertyValues) {
+        if (
+            !_changedProperties.has("locale") &&
+            !_changedProperties.has("_resolvedLocale") &&
+            !_changedProperties.has("type") &&
+            !_changedProperties.has("display")
+        ) {
+            return;
+        }
+
+        this._formatter = new Intl.DisplayNames(this._resolvedLocale, {
+            type: this.type,
+            ...(this.display && { style: this.display }),
+        });
+    }
+}
+
+/**
+ * Add the custom element to HTMLElementTagNameMap for typing.
+ */
+declare global {
+    interface HTMLElementTagNameMap {
+        "i18n-display": Display;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Currency } from "./currency.js";
 export { Datetime } from "./datetime.js";
+export { Display } from "./display-name.js";
 export { Duration } from "./duration.js";
 export { NumberFormat } from "./number.js";
